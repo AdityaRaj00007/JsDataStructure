99947f0ba151633c2b26d2fe1729519812f560f9
AdityaRaj00007 <rajgupta49102@gmail.com>
2025-03-05 14:58:20 +0530

    [Aditya] Add . Program to find the repeated digit from 0-100

---
+++ arrays/RepeatedDigit.js
@@ -0,0 +1,16 @@
+//method to find repeated digit
+function findRepeatedDigitNumbers(start, end){
+    let repeatedNumbers = [];
+
+    for (let i = start; i <= end; i++) {
+        let tens = Math.floor(i / 10);
+        let ones = i % 10;
+        if (tens === ones) {
+            repeatedNumbers.push(i);
+        }
+    }
+
+    return repeatedNumbers;
+}
+let result = findRepeatedDigitNumbers(1, 99);
+console.log(result);
